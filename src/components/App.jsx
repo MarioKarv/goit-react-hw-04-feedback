7a382acc669d0636c2e3a3512130d95bef37f09c
MarioKarv <mariook0204@gmail.com>
2023-05-17 11:59:19 +0300

    initial changes #2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+import { FeedbackMainDiv } from './FeedbackMainDiv/FeedbackMainDiv';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  leaveFeedback = option => {
+    this.setState(state => ({
+      [option]: state[option] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, option) => acc + option, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100) || 0;
+  };
+
+  render() {
+    const { good, bad, neutral } = this.state;
+    return (
+      <FeedbackMainDiv>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.leaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </FeedbackMainDiv>
+    );
+  }
+}
---
+++ src/components/FeedbackMainDiv/FeedbackMainDiv.jsx
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+import css from './FeedbackMainDiv.module.css'
+export function FeedbackMainDiv({ children }) {
+    return <div className={css.container}>{children}</div>;
+}
+
+FeedbackMainDiv.propTypes = {
+  children: PropTypes.node.isRequired,
+};
